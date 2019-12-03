1e3f488fdb07a3fd0d05278080f9bee90b8e7ab2
tansiewling-hotmail <prudence_tan@hotmail.com>
2019-12-03 12:14:25 +0800

    Updated methods in memberships_users controller

--- app/controllers/memberships_users_controller.rb
+++ app/controllers/memberships_users_controller.rb
@@ -1,15 +1,19 @@
 class MembershipsUsersController < ApplicationController
+
   before_action :set_memberships_user, only: [:show, :edit, :update, :destroy]
+
+  before_action :authenticate_user!
 
   # GET /memberships_users
   # GET /memberships_users.json
   def index
-    @memberships_users = MembershipsUser.all
+    @memberships_users = MembershipsUser.where(user_id: current_user.id)
   end
 
   # GET /memberships_users/1
   # GET /memberships_users/1.json
   def show
+    # @memberships_user = MembershipsUser.find(params[:id])
   end
 
   # GET /memberships_users/new
@@ -19,12 +23,22 @@ class MembershipsUsersController < ApplicationController
 
   # GET /memberships_users/1/edit
   def edit
+    @memberships_user = MembershipsUser.find(params[:id])
   end
 
   # POST /memberships_users
   # POST /memberships_users.json
   def create
     @memberships_user = MembershipsUser.new(memberships_user_params)
+    @memberships_user.membership_id = 2
+    @memberships_user.user_id = current_user.id
+
+    # @memberships = Membership.all
+    # puts "Last index >>>>>"
+    # counter = @memberships.size()
+    # puts counter
+
+    puts @memberships_user_params.inspect
 
     respond_to do |format|
       if @memberships_user.save
@@ -40,6 +54,11 @@ class MembershipsUsersController < ApplicationController
   # PATCH/PUT /memberships_users/1
   # PATCH/PUT /memberships_users/1.json
   def update
+    @memberships_user = MembershipsUser.find(params[:id])
+    @memberships_user.user_id = current_user.id
+
+    puts @memberships_user.inspect
+
     respond_to do |format|
       if @memberships_user.update(memberships_user_params)
         format.html { redirect_to @memberships_user, notice: 'Memberships user was successfully updated.' }
@@ -69,6 +88,6 @@ class MembershipsUsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def memberships_user_params
-      params.require(:memberships_user).permit(:user_id, :membership_id)
+      params.require(:memberships_user).permit(:store, :status, :points, :rewards, :membership_id)
     end
 end
--- app/controllers/onepage_controller.rb
+++ app/controllers/onepage_controller.rb
@@ -1,7 +1,5 @@
 class OnepageController < ApplicationController
 
-  before_action :authenticate_user!
-
   def index
   end
 end
